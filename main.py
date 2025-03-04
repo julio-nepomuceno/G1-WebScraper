9ce4362bc8addb6e380dcf5c7f3142c95c42b8da
Júlio Nepomuceno <juliocosta3898@gmail.com>
2025-03-04 00:20:34 -0300

    criei o main

---
+++ main.py
@@ -0,0 +1,19 @@
+import requests
+from termcolor import colored
+from bs4 import BeautifulSoup
+
+link = 'https://g1.globo.com/'
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36'
+}
+requisicao = requests.get(link, headers=headers)
+site = BeautifulSoup(requisicao.text, 'html.parser')
+
+principais_noticias = site.find_all('a', class_="feed-post-link gui-color-primary gui-color-hover")
+for i, noticia in enumerate(principais_noticias[:4], 1):
+    titulo = noticia.text.strip().upper()
+    link = noticia["href"]
+    print(colored(f'{i}° {titulo}', 'light_yellow'))
+    print('Link:', end=' ')
+    print(colored(f'{link}', 'light_cyan'))
+print(colored('© Júlio', 'dark_grey'))
